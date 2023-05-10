0f3ab035c1200373c97d460918485fad26beca55
mg-yamanedaiki <d.yamane@mountain-gorilla.co.jp>
2023-05-10 13:37:05 +0900

    コマンドラインから入力できるように変更

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import argparse
 from yt_dlp import YoutubeDL
-#オプションを指定（最高音質の音声をmp3形式でダウンロードする）
-option = {
-        'outtmpl' : '%(title)s.%(ext)s',
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',  
-            'preferredcodec': 'mp4',  #変換したい形式を指定
-            'preferredquality': '192' #ビットレートを指定
-        }]
-    }
-ydl = YoutubeDL({})
-result = ydl.download('https://www.youtube.com/watch?v=kwfPxOM415o&ab_channel=ChilliBeans.')
-print(result)
+
+def get_args():
+    psr  = argparse.ArgumentParser(
+    prog='youtube_dl',
+    usage='python main.py url [--type]',
+    description='youtubeの動画ダウンロード'
+    )
+    # add_argumentメソッドを使って、コマンドラインから引数を受け取る処理を作成する
+    psr.add_argument('url', help='youtube url')
+    psr.add_argument('-t', '--type', help='拡張子')
+    return psr.parse_args()
+
+
+def main():
+    args = get_args()
+    option = {}
+    if args.type:
+        option = {
+            'outtmpl': '%(title)s.%(ext)s',
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',  
+                'preferredcodec': args.type,  #変換したい形式を指定
+                'preferredquality': '192' #ビットレートを指定
+            }]
+        }
+    ydl = YoutubeDL(option)
+    result = ydl.download(args.url)
+    print(result)
+
+if __name__ == '__main__':
+    main()
